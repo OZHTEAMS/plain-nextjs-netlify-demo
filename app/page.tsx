7f61fd69eb020148aafec6220a63156f1be15be8
OZHTEAMS <ozhmanagement@gmail.com>
2026-03-10 17:01:00 +0000

    Conectando Prisma con la UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export const dynamic = "force-dynamic"; // This disables SSG and ISR
 
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
